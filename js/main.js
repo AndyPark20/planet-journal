fe8fe98cb309b7ef3c955c267fb301bfcf761977
Andy Park <apark1214@gmail.com>
2020-12-01 15:25:37 -0800

    added submit event on the form. also changed button to input type submt

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 var $urlInput = document.querySelector('#avatarUrl');
 var $image = document.querySelector('img');
+var $userSubmitBtn = document.querySelector('form');
 
 $urlInput.addEventListener('input', function (e) {
 
@@ -9,5 +10,10 @@ $urlInput.addEventListener('input', function (e) {
   } else {
     $image.setAttribute('src', e.target.value);
   }
+
+});
+
+$userSubmitBtn.addEventListener('submit', function (e) {
+  e.preventDefault();
 
 });
