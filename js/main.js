c7a3c7b264147ea9c00348fb50da2a82dfeb1a7a
Andy Park <apark1214@gmail.com>
2020-12-01 15:13:25 -0800

    updated avatar preview image's src when the avatar url input changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 
 var $urlInput = document.querySelector('#avatarUrl');
+var $image = document.querySelector('img');
 
-$urlInput.addEventListener('input', function () {
+$urlInput.addEventListener('input', function (e) {
+
+  if ($urlInput.value === '') {
+    $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  } else {
+    $image.setAttribute('src', e.target.value);
+  }
 
 });
