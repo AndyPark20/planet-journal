cdb38577e9161f8cbf2abd50737e99c0aec6fb74
Andy Park <apark1214@gmail.com>
2021-02-24 19:56:09 -0800

    add-entries

--- js/data.js
+++ js/data.js
@@ -10,5 +10,32 @@ var data = {
     bio: `Sure, Jupiter has constant hurricanes of gas and Mars may even have some frozen water … but Earth is the only planet in our solar
     system capable of creating rainbows – so far as we know. That’s because Earth is the only planet with consistent liquid precipitation and direct sunlight.`
   },
-  entries: []
+  entries: [
+    {
+      note: 'Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, slightly more massive than its near-twin Uranus.',
+      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg',
+      title: 'Neptune'
+    },
+    {
+      note: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two and a half times that of all the other planets in the Solar System combined.',
+      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg',
+      title: 'Jupiter'
+    },
+    {
+      note: 'Venus is the second planet from the Sun.It is named after the Roman goddess of love and beauty.As the brightest natural object in Earth\'s night sky after the Moon, Venus can cast shadows and can be, on rare occasion, visible to the naked eye in broad daylight.',
+      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/PIA23791-Venus-NewlyProcessedView-20200608.jpg',
+      title: 'Venus'
+    },
+    {
+      note: `Earth is the third planet from the Sun and the only astronomical object known to harbor life. About 29% of Earth's surface is land consisting of continents and islands. The remaining 71% is covered with water, mostly by oceans but also by lakes, rivers, and other fresh water, which together constitute the hydrosphere. Much of Earth's polar regions are covered in ice. Earth's outer layer is divided into several rigid tectonic plates that migrate across the surface
+    over many millions of years. Earth's interior remains active with a solid iron inner core, a liquid outer core that generates Earth's magnetic field, and a convecting mantle that drives plate tectonics.`,
+      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/1024px-The_Earth_seen_from_Apollo_17.jpg',
+      title: 'Earth'
+    },
+    {
+      note: 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the "Red Planet',
+      photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg',
+      title: 'Mars'
+    }
+  ]
 };
