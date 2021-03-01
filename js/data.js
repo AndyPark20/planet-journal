f7f28eb99d712f2848aef5b7e4b0564c29debf35
Andy Park <apark1214@gmail.com>
2021-03-01 13:16:53 -0800

    fix the extra space default info

--- js/data.js
+++ js/data.js
@@ -7,8 +7,7 @@ var data = {
     fullName: 'AP2021',
     location: 'Orange County, CA',
     avatarUrl: 'https://user-images.githubusercontent.com/69870979/109091908-7c8cb680-76ca-11eb-8449-e04bb195dd08.jpg',
-    bio: `Sure, Jupiter has constant hurricanes of gas and Mars may even have some frozen water … but Earth is the only planet in our solar
-    system capable of creating rainbows – so far as we know. That’s because Earth is the only planet with consistent liquid precipitation and direct sunlight.`
+    bio: 'Sure, Jupiter has constant hurricanes of gas and Mars may even have some frozen water … but Earth is the only planet in our solar system capable of creating rainbows – so far as we know. That’s because Earth is the only planet with consistent liquid precipitation and direct sunlight.'
   },
   entries: [
     {
